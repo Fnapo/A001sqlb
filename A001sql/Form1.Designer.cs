308ce26e35a0d5ace8907718659adcbccbef1ec2
Fnapo <36490842+Fnapo@users.noreply.github.com>
2022-06-13 21:59:07 +0200

    Uno de prueba

---
+++ A001sql/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace A001sql
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código generado por el Diseñador de Windows Forms
+
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.buttonProbar = new System.Windows.Forms.Button();
+			this.errorProviderControl = new System.Windows.Forms.ErrorProvider(this.components);
+			this.controlErrorBBDD = new LibreriaControles.ControlErrorCentrado();
+			this.controlErrorServidor = new LibreriaControles.ControlErrorCentrado();
+			this.buttonGuardar = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.errorProviderControl)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// buttonProbar
+			// 
+			this.buttonProbar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(120)))));
+			this.buttonProbar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.buttonProbar.ForeColor = System.Drawing.Color.White;
+			this.buttonProbar.Location = new System.Drawing.Point(133, 210);
+			this.buttonProbar.Name = "buttonProbar";
+			this.buttonProbar.Size = new System.Drawing.Size(200, 60);
+			this.buttonProbar.TabIndex = 6;
+			this.buttonProbar.Text = "PROBAR CONEXIÓN";
+			this.buttonProbar.UseVisualStyleBackColor = false;
+			this.buttonProbar.Click += new System.EventHandler(this.Button1_Click);
+			// 
+			// errorProviderControl
+			// 
+			this.errorProviderControl.ContainerControl = this;
+			// 
+			// controlErrorBBDD
+			// 
+			this.controlErrorBBDD.Location = new System.Drawing.Point(100, 90);
+			this.controlErrorBBDD.Margin = new System.Windows.Forms.Padding(5);
+			this.controlErrorBBDD.Name = "controlErrorBBDD";
+			this.controlErrorBBDD.Size = new System.Drawing.Size(460, 60);
+			this.controlErrorBBDD.TabIndex = 8;
+			this.controlErrorBBDD.TextError = "";
+			this.controlErrorBBDD.TextEtiqueta = "Base de Datos:";
+			this.controlErrorBBDD.TextInput = "";
+			// 
+			// controlErrorServidor
+			// 
+			this.controlErrorServidor.Location = new System.Drawing.Point(100, 20);
+			this.controlErrorServidor.Margin = new System.Windows.Forms.Padding(4);
+			this.controlErrorServidor.Name = "controlErrorServidor";
+			this.controlErrorServidor.Size = new System.Drawing.Size(460, 60);
+			this.controlErrorServidor.TabIndex = 7;
+			this.controlErrorServidor.TextError = "";
+			this.controlErrorServidor.TextEtiqueta = "Servidor:";
+			this.controlErrorServidor.TextInput = "";
+			// 
+			// buttonGuardar
+			// 
+			this.buttonGuardar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(120)))), ((int)(((byte)(60)))));
+			this.buttonGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.buttonGuardar.ForeColor = System.Drawing.Color.White;
+			this.buttonGuardar.Location = new System.Drawing.Point(393, 210);
+			this.buttonGuardar.Name = "buttonGuardar";
+			this.buttonGuardar.Size = new System.Drawing.Size(200, 60);
+			this.buttonGuardar.TabIndex = 9;
+			this.buttonGuardar.Text = "GUARDAR DATOS";
+			this.buttonGuardar.UseVisualStyleBackColor = false;
+			this.buttonGuardar.Click += new System.EventHandler(this.ButtonGuardar_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(727, 304);
+			this.Controls.Add(this.buttonGuardar);
+			this.Controls.Add(this.controlErrorBBDD);
+			this.Controls.Add(this.controlErrorServidor);
+			this.Controls.Add(this.buttonProbar);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Margin = new System.Windows.Forms.Padding(4);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.errorProviderControl)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Button buttonProbar;
+		private LibreriaControles.ControlErrorCentrado controlErrorServidor;
+		private System.Windows.Forms.ErrorProvider errorProviderControl;
+		private LibreriaControles.ControlErrorCentrado controlErrorBBDD;
+		private System.Windows.Forms.Button buttonGuardar;
+	}
+}
+
---
+++ A001sql/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using ValidarForms;
+
+namespace A001sql
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+
+			PrepararInputs();
+			buttonGuardar.Enabled = false;
+		}
+
+		private void PrepararInputs()
+		{
+			LeerConfiguracion(out string servidor, out string baseDatos);
+			controlErrorServidor.TextEtiqueta = "Servidor:";
+			controlErrorServidor.TextError = "";
+			controlErrorServidor.TextInput = servidor;
+			controlErrorBBDD.TextEtiqueta = "Base de Datos:";
+			controlErrorBBDD.TextError = "";
+			controlErrorBBDD.TextInput = baseDatos;
+		}
+
+		private void LeerConfiguracion(out string servidor, out string baseDatos)
+		{
+			servidor = ConfigurationManager.AppSettings["Servidor"] ?? "";
+			baseDatos = ConfigurationManager.AppSettings["BaseDatos"] ?? "";
+			if (servidor != "")
+			{
+				servidor = EncriptarTexto.DesEncriptar(servidor);
+			}
+			if (baseDatos != "")
+			{
+				baseDatos = EncriptarTexto.DesEncriptar(baseDatos);
+			}
+		}
+
+		private void Button1_Click(object sender, System.EventArgs e)
+		{
+			bool correctos = ValidarCampos();
+
+			buttonGuardar.Enabled = false;
+			if (correctos)
+			{
+				string cadenaConexion = $"Persist Security Info=False;Integrated Security=true;" +
+					$"Initial Catalog={controlErrorBBDD.TextInput};server={controlErrorServidor.TextInput}";
+
+				using (SqlConnection conexion = new SqlConnection(cadenaConexion))
+				{
+					Cursor = Cursors.WaitCursor;
+					try
+					{
+						conexion.Open();
+						Cursor = Cursors.Default;
+						MessageBox.Show("Puedes cerrar la ventana de conexión.", "Conexión correcta ...",
+							MessageBoxButtons.OK);
+						buttonGuardar.Enabled = true;
+					}
+					catch (Exception)
+					{
+						Cursor = Cursors.Default;
+						MessageBox.Show("Algún parámetro no es correcto.", "Conexión fallida ...",
+							MessageBoxButtons.OK);
+					}
+				}
+			}
+		}
+
+		private bool ValidarCampos()
+		{
+			bool salida = FormsValidacion.ValidarLongitudTexto(errorProviderControl,
+				controlErrorServidor, "Servidor", 1, 55);
+
+			salida &= FormsValidacion.ValidarLongitudTexto(errorProviderControl,
+				controlErrorBBDD, "Base de Datos", 1, 55);
+
+			return salida;
+		}
+
+		private void ButtonGuardar_Click(object sender, EventArgs e)
+		{
+			Configuration configuracion =
+				ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+			var settings = configuracion.AppSettings.Settings;
+			string servidor = controlErrorServidor.TextInput, baseDatos = controlErrorBBDD.TextInput;
+
+			servidor = EncriptarTexto.Encriptar(servidor);
+			baseDatos = EncriptarTexto.Encriptar(baseDatos);
+			if (settings.Count == 0)
+			{
+				settings.Add("Servidor", servidor);
+				settings.Add("BaseDatos", baseDatos);
+			}
+			else
+			{
+				if (settings["Servidor"] is null)
+				{
+					settings.Add("Servidor", servidor);
+				}
+				else
+				{
+					settings["Servidor"].Value = servidor;
+				}
+				if (settings["BaseDatos"] is null)
+				{
+					settings.Add("BaseDatos", baseDatos);
+				}
+				else
+				{
+					settings["BaseDatos"].Value = baseDatos;
+				}
+			}
+
+			configuracion.Save(ConfigurationSaveMode.Full, true);
+			ConfigurationManager.RefreshSection("appSettings");
+		}
+	}
+}
